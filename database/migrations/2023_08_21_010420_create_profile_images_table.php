a829f2f5a1ccab9195300e11d6589a0d97d55a47
wistox07 <fodelosrios@gmail.com>
2023-08-27 07:23:36 -0500

    Gato

--- database/migrations/2023_08_21_010420_create_profile_images_table.php
+++ database/migrations/2023_08_21_010420_create_profile_images_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string("name");
             $table->string('route'); // Ruta o URL de la imagen
             $table->timestamps();
+            //gato
         });
     }
 
